c33ea5e926a70942c30affbff42d75d636b97857
Wendel Cutrim <wendelcutrim@hotmail.com>
2024-05-26 20:44:00 -0300

    feat: Create script to build and deploy project on github pages, create a regex to validate the plate car input, create a function and global styles to prevent default the refresh on mobile devices, remove the home path route and back to the default path because the route problems on deploy now are fixed and change the routerlink path on erro page to redirect the default path and dont call the path home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { ErrorComponent } from './pages/error/error.component';
 import { PrintComponent } from './pages/print/print.component';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: '', component: HomeComponent },
+    // { path: 'home', component: HomeComponent },
     { path: 'error', component: ErrorComponent },
     { path: 'print', component: PrintComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    // { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/error' },
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,19 @@ export class AppComponent implements OnInit {
     constructor(private router: Router) {}
 
     ngOnInit(): void {
-        // this.router.navigate(['/home']);
+        this.disableNavigationReloadOnMobileDevices();
+    }
+
+    isMobileDevice() {
+        const mobileDevicesUserAgentsRegExp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+        return mobileDevicesUserAgentsRegExp.test(navigator.userAgent);
+    }
+
+    disableNavigationReloadOnMobileDevices() {
+        const isMobileDevice = this.isMobileDevice();
+        console.log('isMobileDevice: ', isMobileDevice);
+        if (isMobileDevice) {
+            (window as any).navigation.reload = () => {};
+        }
     }
 }
